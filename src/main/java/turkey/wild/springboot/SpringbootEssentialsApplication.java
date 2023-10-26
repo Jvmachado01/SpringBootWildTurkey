ddb2b5f1884313dd41298ae5ddde560aaacd4b16
Jvmachado01 <joaommachado89@outlook.com>
2023-10-25 21:15:36 -0300

    Override handler method of the Spring on exceptions=RestExceptionHandler

--- src/main/java/turkey/wild/springboot/SpringbootEssentialsApplication.java
+++ src/main/java/turkey/wild/springboot/SpringbootEssentialsApplication.java
@@ -3,7 +3,7 @@ package turkey.wild.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// BRANCH SIX ========================> 6 <=================/
+// BRANCH SEVEN ========================> 7 <=================/
 
 @SpringBootApplication
 public class  SpringbootEssentialsApplication {
--- src/main/java/turkey/wild/springboot/handler/RestExceptionHandler.java
+++ src/main/java/turkey/wild/springboot/handler/RestExceptionHandler.java
@@ -1,14 +1,21 @@
 package turkey.wild.springboot.handler;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import turkey.wild.springboot.exception.BadRequestException;
 import turkey.wild.springboot.exception.BadRequestExceptionDetails;
+import turkey.wild.springboot.exception.ExceptionDetails;
 import turkey.wild.springboot.exception.ValidationExceptionDetails;
 
 import java.time.LocalDateTime;
@@ -19,12 +26,12 @@ import java.util.stream.Collectors;
 @ControllerAdvice
 @Log4j2
 // We can have several types of exceptions within this class. So the name RestExceptionHandler.
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     // We are informing the controller in case there is "Bad Request"
     // the method "handlerBadRequestException" will be called.
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException bre) {
+    public ResponseEntity<BadRequestExceptionDetails> handleBadRequestException(BadRequestException bre) {
         return new ResponseEntity<>(
                 BadRequestExceptionDetails.builder()
                         .title("Bad Reques Exception. Check the Documentation")
@@ -36,10 +43,9 @@ public class RestExceptionHandler {
 
     }
 
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException(
-            MethodArgumentNotValidException exception) {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
 
         String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
@@ -56,6 +62,21 @@ public class RestExceptionHandler {
                         .fieldsMessage(fieldsMessage)
                         .build(), HttpStatus.BAD_REQUEST);
 
+    }
+
+    @Nullable
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        ExceptionDetails exceptionDetails = ExceptionDetails.builder()
+                .title(ex.getCause().getMessage())
+                .status(status.value())
+                .details(ex.getMessage())
+                .developerMessage(ex.getClass().getName())
+                .timestamp(LocalDateTime.now())
+                .build();
+        return handleExceptionInternal(ex, exceptionDetails, headers, status, request);
     }
 
 
